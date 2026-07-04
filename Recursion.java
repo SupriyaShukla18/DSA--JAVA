bc69cea356ae498ba7dea8c93414d78fb8fad58c
Supriya Shukla <shuklasupriya251@gmail.com>
2026-07-04 20:55:43 +0530

    Added recursion basics

---
+++ Recursion.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Recursion{
+    public static void main(String [] main){
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        printnum(n);
+    }
+    public static void printnum(int n){
+     if(n==0){
+        return;
+     }
+     System.out.println(n);
+
+        printnum(n-1);
+
+    }
+}
